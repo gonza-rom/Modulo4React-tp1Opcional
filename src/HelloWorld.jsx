bef2185252271c2a5dabd6402a451625a05d7055
gonza <gonzalorom2001@gmail.com>
2025-02-11 19:45:52 -0300

    TpOpcional

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HelloWorld.css"; // Importamos el archivo de estilos
+
+const HelloWorld = () => {
+  return <h1 className="hello-text">¡Hola, Mundo!</h1>;
+};
+
+export default HelloWorld;
